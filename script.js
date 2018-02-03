cd3bb929b84870fc2e3feef02b8d0647791a012e
Winston Lan <winston.chunhua.lan@gmail.com>
2018-02-03 08:43:52 -0800

    Worked on js functions
    can read/write data to firebase.

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+/**
+	Setup Firebase Database.
+*/
 var config = {
-    apiKey: "AIzaSyDfm9YsOktUVw29r1V1g19-E9VDUhKDOik",
-    authDomain: "fitfriends-5d536.firebaseapp.com",
-    databaseURL: "https://fitfriends-5d536.firebaseio.com",
-    projectId: "fitfriends-5d536",
-    storageBucket: "fitfriends-5d536.appspot.com",
-    messagingSenderId: "294476758109"
-  };
-  firebase.initializeApp(config);
-
+	apiKey: "AIzaSyDfm9YsOktUVw29r1V1g19-E9VDUhKDOik",
+	authDomain: "fitfriends-5d536.firebaseapp.com",
+	databaseURL: "https://fitfriends-5d536.firebaseio.com",
+	projectId: "fitfriends-5d536",
+	storageBucket: "fitfriends-5d536.appspot.com",
+	messagingSenderId: "294476758109"
+};
+firebase.initializeApp(config);
 var database = firebase.database();
-var userId = "helloWorld";
 
-function writeUserData() {
-  firebase.database().ref('friends/' + userId).set({
-    name: "John Doe",
-    gender: "M",
-    availability: "bleh",
-    pref: "ANYTHING",
-    contact: "123456789"
-  });
+// GLOBAL VARS
+var user = {
+	userId: "0000",
+	userName: "John Doe",
+	userGender: "M"
+};
+
+/**
+	Write a friend's information to the database.
+*/
+function writeFriend(fName, fGender, fAvail, fPref, fContact) {
+	firebase.database().ref('friends/' + userId).set({
+		name: fName,
+		gender: fGender,
+		availability: fAvail,
+		pref: fPref,
+		contact: fContact
+	});
 }
 
-writeUserData();
+/**
+	Get all the friends in the database, that match with the availability preferences.
+*/
+function getFriends() {
+	firebase.database().ref("friends/").once('value').then(function(snapshot) {
+		console.log(snapshot.val());
+  	});
+}
+getFriends();
